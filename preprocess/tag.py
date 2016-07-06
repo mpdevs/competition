664b141572c6238d416d57e17529ad355b8dd517
panjunjun <41317139@qq.com>
2016-07-06 18:20:24 +0800

    添加注释

--- preprocess/tag.py
+++ preprocess/tag.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*
 # __author__: huang_yanhua
+"""
+打标签主程序
+留坑:
+爬虫程序会对取回来的数据进行逗号替换，英文逗号会替换成中文逗号
+"""
 import os
 import MySQLdb
 from datetime import datetime
@@ -15,8 +20,14 @@ BASE_DIR = os.path.join(os.path.dirname(__file__), 'dicts')
 
 
 def process_tag(industry, table_name):
- 
+    """
+    打标签需要打两个不同的字段： 1. 品牌 2. 属性
+    :param industry:
+    :param table_name:
+    :return:
+    """
     # 词库文件列表，unix style pathname pattern
+    # 词库来源： 坚果云/词库/行业/行业品牌词库(最新版)
     brand_list = BASE_DIR + u'/brand/'+industry+'/*.txt'  # 品牌词库
 
     # exclusive_list 互斥属性类型：以商品详情为准的标签类型
@@ -27,29 +38,35 @@ def process_tag(industry, table_name):
     portal = MySQLdb.Connect(host=host, user=user, passwd=pwd, db='mp_portal', charset='utf8')
 
     # 选取商品数据
-    query = """
-                SELECT ItemID, CategoryID, concat_ws(' ',ItemSubTitle,ItemName) AS Title,
-                ItemAttrDesc AS Attribute, concat_ws(' ',ShopName,ItemSubTitle,ItemName) AS ShopNameTitle
-                FROM %s WHERE NeedReTag='y';
-                """ % table_name
-        
+    # 商品在打完标签后会对NeedReTag做更新
+    # 目前NeedReTag只对item表有效, sales表以后要加字段
+    # 全更新去数据库更新NeedReTag字段
+    item_info_query = """SELECT ItemID, CategoryID, concat_ws(' ',ItemSubTitle,ItemName) AS Title,
+    ItemAttrDesc AS Attribute, concat_ws(' ',ShopName,ItemSubTitle,ItemName) AS ShopNameTitle
+    FROM {0} WHERE NeedReTag='y';""".format(table_name)
+
     update_sql = """UPDATE {0}
     SET TaggedItemAttr=%s, NeedReTag='n', TaggedBrandName=%s
     WHERE ItemID=%s ;""".format(table_name)
-        
+
     print '{} Loading data ...'.format(datetime.now())
-    data = pd.read_sql_query(query, connect) 
-    
+    data = pd.read_sql_query(item_info_query, connect)
+
     n = len(data)
     if n > 0:
-        print '{} Preprocess ...'.format(datetime.now())       
-        batch = 20000  # 20000的整数倍    
+        print '{} Preprocess ...'.format(datetime.now())
+        batch = 20000  # 20000的整数倍
+        # 商店名去空格
+        # 天猫前段输入框只支持空格和其他标点符号，不能输入\t \r \n
         data['ShopNameTitle'] = data['ShopNameTitle'].str.replace(' ', '')
+        # 属性去空格
         data['Attribute'] = data['Attribute'].str.replace(' ', '')
-        
+
         # 标签准备
+        # brand_preparation 格式 {"正则表达式", "品牌"}
         brand_preparation = tagging_ali_brands_preparation(brand_list)
 
+        # region 合并成一个Query
         industry_id_query = "SELECT IndustryID FROM industry WHERE DBName='{}'".format(industry)
         industry_id = pd.read_sql_query(industry_id_query, portal)['IndustryID'].values[0]
 
@@ -58,42 +75,57 @@ def process_tag(industry, table_name):
         WHERE IsTag='y'
         AND
         IndustryID={}""".format(industry_id)
+        # endregion
         tag_dicts = pd.read_sql_query(tag_dicts_query, portal)
 
+        # tag_preparation 打标签用和 brand_preparation类似
         tag_preparation = dict()
-             
+
         for cid in tag_dicts['CID'].unique():
+            # {"CID": {("attr_name", "display_name", "attr_flag"): list}}
+            # dict(dict[tuple](list))
+            # 抓取的数据会有截尾逗号和空格
+            # x[1] = CID
+            # x[2] = Attrname
+            # x[3] = DisplayName
+            # x[4] = AttrValue
+            # x[5] = Flag
             tag_preparation[int(cid)] = {(x[1], x[2], x[4]): x[3].rstrip(',').replace(' ', '').split(',')
+                                         # type(x) = ndarray
                                          for x in tag_dicts[tag_dicts['CID'] == cid].values}
 
         print u'Total number of data: {}, batch_size = {}'.format(n, batch)
-        
-        split_df = [data.iloc[j*batch:min((j+1)*batch, n)] for j in xrange(int(ceil(float(n)/batch)))]        
+
+        # 切分数据
+        # split_df = map(pd.DataFrame(), np.array_split(data.values, n/batch))
+        split_df = [data.iloc[j*batch:min((j+1)*batch, n)] for j in xrange(int(ceil(float(n)/batch)))]
         cursor = connect.cursor()
         for j in xrange(len(split_df)):
             print '{} Start batch {}'.format(datetime.now(), j+1)
             batch_data = split_df[0]
-            
-            print '{} Tagging brands ...'.format(datetime.now())
 
+            print '{} Tagging brands ...'.format(datetime.now())
+            # Attribute 是商品详情，通过爬虫直接存好
+            # 打标签的同时把品牌名加入到属性的json里
+            # type(brand) = list
             brand = tagging_ali_brands(batch_data['Attribute'].values,
                                        batch_data['ShopNameTitle'].values,
                                        brand_preparation)
-            
+
             print '{} Tagging features ...'.format(datetime.now())
             label = tagging_ali_items(batch_data, tag_preparation, exclusive_list)
-            
-            item_ids = map(int, batch_data['ItemID'].values)  
-            
+
+            item_ids = map(int, batch_data['ItemID'].values)
+
             update_items = zip(label, brand, item_ids)
 
             print u'{} Writing this batch to database ...'.format(datetime.now())
             cursor.executemany(update_sql, update_items)
             connect.commit()
             del split_df[0]
-           
+
         connect.close()
         print u'{} Done!'.format(datetime.now())
 
     else:
-        print u'Data in %s had been tagged!' % table_name
+        print u'Data in {} had been tagged!'.format(table_name)
--- preprocess/tag_process.py
+++ preprocess/tag_process.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # __author__: huang_yanhua
+"""
+打标签工具包
+"""
+import json
 import re
 from numpy import array_split
 from glob import glob
@@ -32,22 +36,28 @@ def tagging_ali_brands_preparation(brands_list):
             无法读取以下词库文件，请转码UTF-8后再次尝试。\n\n***\n\n{}
             """.format('\n'.join(error_tag_files))
         raise SystemExit()
-    
+
+    # 打品牌，优先打长的词，一样长就按照字典的排序来操作
     w2b.sort(key=lambda x: (len(x[0]), x[0]), reverse=True)
     ordered_dict = OrderedDict()
+    # w2b的key是正则表达式，value是品牌名
+    # 正则表达式的用处是对品牌名进行遍历，做映射
     for _ in w2b:
         ordered_dict[re.compile(_[0].decode('utf8'), re.IGNORECASE)] = _[1]
     
     return ordered_dict
 
 
+# 找品牌
 def tagging_ali_brands(att, st_list, w2b, split=10000):
+    # 多线程处理
     pool = Pool()
-    indices = 2 if len(att) <= split else len(att) / split 
-    att_list = array_split(att, indices)
+    batch = 2 if len(att) <= split else len(att) / split
+    att_list = array_split(att, batch)
     # shopname and title st_list
-    splited_st_list = array_split(st_list, indices)
-    w2b_list = [w2b] * indices 
+    # 每个线程处理一批商品名和属性
+    splited_st_list = array_split(st_list, batch)
+    w2b_list = [w2b] * batch
     result = pool.map(brand_core, zip(att_list, splited_st_list, w2b_list))
     pool.close()
     pool.join()
@@ -58,23 +68,28 @@ def tagging_ali_brands(att, st_list, w2b, split=10000):
 
 
 def brand_core(a_zip):
+    #  att=商品详情 np.array(string)
     att = a_zip[0]
+    # st_list=店名和title list
     st_list = a_zip[1]
+    # dict
     w2b = a_zip[2]
-    amt = len(att)
-    brand_list = [None] * amt
+    # 每个商品映射一个品牌
+    brand_list = [None] * len(att)
 
     for i, x in enumerate(tqdm(att)):       
         if x is not None:
-            n = max(x.find(u'品牌:'), x.find(u'品牌：')) + 3           
+            n = max(x.find(u'品牌:'), x.find(u'品牌：')) + 3
+            # 商品详情有品牌信息
             if n != 2:
                 t = x[n:x.find(u',', n)]
                 for p in w2b.iterkeys():
                     if p.search(t):
                         brand_list[i] = w2b[p]
                         break
-        
+
         st = st_list[i]
+        # 在商品详情里没有品牌信息，则去点名和title里匹配
         if brand_list[i] is None and st is not None:
             for p in w2b.iterkeys():
                 if p.search(st):
@@ -84,40 +99,61 @@ def brand_core(a_zip):
     return brand_list
 
 
+# 属性打标签
 def tagging_ali_items(data, tag_preparation, exclusive_list):
-    import json
+    """
+    属性匹配的时候 str.split 是大小写敏感的
+    pandas.Series.str.contains 支持正则表达式
+    可能会改
+    :param data:
+    :param tag_preparation:
+    :param exclusive_list:
+    :return: list
+    """
     result = []
     for i in tqdm(data.index):
-        idict = dict()
+        row_dict = dict()
         try:
+            # use_dict  商品对应的品类的标签词库
             use_dict = tag_preparation[int(data['CategoryID'][i])]
         except KeyError:
             print 'CategoryID {} don\'t have attrdict.'.format(data['CategoryID'][i])
+            # 会影响竟品的数据抽取
             result.append('')
             continue
             
         attr = data['Attribute'][i]
         title = data['Title'][i]
+        # type(name) = tuple
+        # type(values) = list
         for name, values in use_dict.iteritems():
+            # name[2]是FLAG
+            # 阿里和宏原的属性名都不一样
+            # 'A' 是阿里
             if name[2] == 'A':
                 j1 = attr.find(name[1] + u':')
                 if j1 != -1:
                     j2 = attr.find(u'，', j1)
                     t = [value for value in values if attr[j1:j2].find(value) != -1]
                     if t:
-                        idict[name[0]] = t[0] if len(t) == 1 else t
+                        row_dict[name[0]] = t[0] if len(t) == 1 else t
+            # 'M' 是宏原
             elif name[2] == 'M' and name[0] not in exclusive_list:
                 t = [value for value in values if (attr+title).find(value) != -1]
                 if t:
-                    idict[name[0]] = t[0] if len(t) == 1 else t
+                    row_dict[name[0]] = t[0] if len(t) == 1 else t
+            # 'M' 宏原属性会互斥
             elif name[2] == 'M' and name[0] in exclusive_list:
+                # value 是词库 , attr 是网页数据
                 t = [value for value in values if attr.find(value) != -1]
                 t = t if t else [value for value in values if title.find(value) != -1]
                 if t:
-                    idict[name[0]] = t[0] if len(t) == 1 else t
+                    # name[0] 是 AttributeName
+                    # 影响竟品
+                    row_dict[name[0]] = t[0] if len(t) == 1 else t
             else:
                 print 'Unknown type of tag: {}'.format(name[2])
                 raise SystemExit()
         
-        result.append(json.dumps(idict, ensure_ascii=False).replace('"', '\'') if len(idict) else '')
+        result.append(json.dumps(row_dict, ensure_ascii=False).replace('"', '\'') if len(row_dict) else '')
     return result
