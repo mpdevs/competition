b3ca80c103c81a7588352ea6513a071cfb55d0d2
panjunjun <41317139@qq.com>
2016-07-06 16:14:04 +0800

    PEP-8 调整

--- preprocess/tag_process.py
+++ preprocess/tag_process.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-
+# __author__: huang_yanhua
 import re
-import numpy as np
-import pandas as pd
-import itertools as it
 from numpy import array_split
 from glob import glob
 from multiprocessing import Pool
-from datetime import datetime
 from tqdm import tqdm
 from collections import OrderedDict
 
 
 def tagging_ali_brands_preparation(brands_list):
     tags = glob(brands_list)
-   
+    # word to brand w2b
     w2b = []
     error_tag_files = []
     for t in tags:
-        tag_name = t.replace('\\','/').split('/')[-1][:-4]
+        tag_name = t.replace('\\', '/').split('/')[-1][:-4]
         try:
             f = open(t, 'r')
             tl = list(set([w.strip('\t\n\r') for w in f if w != '']))
@@ -26,9 +22,9 @@ def tagging_ali_brands_preparation(brands_list):
             for w in tl:
                 for p in ['\\', '+', '*', '?', '^', '$', '|', '.', '[']:
                     w = w.replace(p, '\\'+p)
-                l.append([w,tag_name])            
+                l.append([w, tag_name])
             w2b += l
-        except UnicodeDecodeError, e:
+        except UnicodeDecodeError:
             error_tag_files.append(t)
             
     if len(error_tag_files) > 0:
@@ -37,34 +33,36 @@ def tagging_ali_brands_preparation(brands_list):
             """.format('\n'.join(error_tag_files))
         raise SystemExit()
     
-    w2b.sort(key=lambda x:(len(x[0]),x[0]), reverse=True)
-    
-    W2B = OrderedDict()
+    w2b.sort(key=lambda x: (len(x[0]), x[0]), reverse=True)
+    ordered_dict = OrderedDict()
     for _ in w2b:
-        W2B[re.compile(_[0].decode('utf8'), re.IGNORECASE)] = _[1]
+        ordered_dict[re.compile(_[0].decode('utf8'), re.IGNORECASE)] = _[1]
     
-    return W2B
+    return ordered_dict
 
-def tagging_ali_brands(att, ST, w2b, split=10000):
+
+def tagging_ali_brands(att, st_list, w2b, split=10000):
     pool = Pool()
     indices = 2 if len(att) <= split else len(att) / split 
     att_list = array_split(att, indices)
-    ST_list = array_split(ST, indices)
+    # shopname and title st_list
+    splited_st_list = array_split(st_list, indices)
     w2b_list = [w2b] * indices 
-    result = pool.map(brand_core, zip(att_list, ST_list, w2b_list))
+    result = pool.map(brand_core, zip(att_list, splited_st_list, w2b_list))
     pool.close()
     pool.join()
     t = []
     for x in result:
         t += x
     return t
-    
+
+
 def brand_core(a_zip):
     att = a_zip[0]
-    ST = a_zip[1]
+    st_list = a_zip[1]
     w2b = a_zip[2]
-    N = len(att)
-    BRAND = [None] * N
+    amt = len(att)
+    brand_list = [None] * amt
 
     for i, x in enumerate(tqdm(att)):       
         if x is not None:
@@ -73,19 +71,20 @@ def brand_core(a_zip):
                 t = x[n:x.find(u',', n)]
                 for p in w2b.iterkeys():
                     if p.search(t):
-                        BRAND[i] = w2b[p]
+                        brand_list[i] = w2b[p]
                         break
         
-        st = ST[i]                 
-        if BRAND[i] is None and st is not None:
+        st = st_list[i]
+        if brand_list[i] is None and st is not None:
             for p in w2b.iterkeys():
                 if p.search(st):
-                    BRAND[i] = w2b[p]
+                    brand_list[i] = w2b[p]
                     break
         
-    return BRAND
-    
-def tagging_ali_items(data, tag_preparation, EXCLUSIVES):
+    return brand_list
+
+
+def tagging_ali_items(data, tag_preparation, exclusive_list):
     import json
     result = []
     for i in tqdm(data.index):
@@ -105,40 +104,20 @@ def tagging_ali_items(data, tag_preparation, EXCLUSIVES):
                 if j1 != -1:
                     j2 = attr.find(u'，', j1)
                     t = [value for value in values if attr[j1:j2].find(value) != -1]
-                    if t: idict[name[0]] = t[0] if len(t) == 1 else t
-            elif name[2] == 'M' and name[0] not in EXCLUSIVES:
+                    if t:
+                        idict[name[0]] = t[0] if len(t) == 1 else t
+            elif name[2] == 'M' and name[0] not in exclusive_list:
                 t = [value for value in values if (attr+title).find(value) != -1]
-                if t: idict[name[0]] = t[0] if len(t) == 1 else t 
-            elif name[2] == 'M' and name[0] in EXCLUSIVES:
+                if t:
+                    idict[name[0]] = t[0] if len(t) == 1 else t
+            elif name[2] == 'M' and name[0] in exclusive_list:
                 t = [value for value in values if attr.find(value) != -1]
                 t = t if t else [value for value in values if title.find(value) != -1]
-                if t: idict[name[0]] = t[0] if len(t) == 1 else t 
+                if t:
+                    idict[name[0]] = t[0] if len(t) == 1 else t
             else:
                 print 'Unknown type of tag: {}'.format(name[2])
                 raise SystemExit()
         
         result.append(json.dumps(idict, ensure_ascii=False).replace('"', '\'') if len(idict) else '')
     return result
-            
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-
-
