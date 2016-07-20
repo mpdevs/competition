dc877c0d45fa56eb2da08b2d46cb110efd4315de
panjunjun <41317139@qq.com>
2016-07-20 13:54:54 +0800

    增加图片展示功能

--- algorithm_tracer/sql_constant.py
+++ algorithm_tracer/sql_constant.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 # __author__ = 'John'
-PREDICT_PAIR_INFO_QUERY = u"""SELECT ItemID, TaggedItemAttr, DiscountPrice, CategoryID, DateRange FROM {0}
-WHERE ItemID IN ({1}, {2}) AND DateRange = '{3}' AND TaggedItemAttr LIKE ',%';"""
+PREDICT_PAIR_INFO_QUERY = u"""SELECT ItemID, TaggedItemAttr, ItemMainPicUrl, DiscountPrice, CategoryID, DateRange
+ FROM {0} WHERE ItemID IN ({1}, {2}) AND DateRange = '{3}' AND TaggedItemAttr LIKE ',%';"""
 
-TRAIN_PAIR_INFO_QUERY = u"SELECT * FROM TaggedItemAttr WHERE ItemID IN ({0}, {1});"
+# TRAIN_PAIR_INFO_QUERY = u"SELECT * FROM TaggedItemAttr WHERE ItemID IN ({0}, {1});"
+TRAIN_PAIR_INFO_QUERY = u"""SELECT a.ItemID, a.TaggedItemAttr, CASE WHEN s.ItemMainPicUrl IS NULL THEN '' ELSE
+s.ItemMainPicUrl END AS  ItemMainPicUrl FROM (SELECT * FROM TaggedItemAttr WHERE ItemID IN ({0}, {1})
+) a LEFT JOIN (SELECT ItemID, ItemMainPicUrl FROM itemmonthlysales2015 WHERE DateRange = '2015-12-01' AND ItemID IN
+({0}, {1})) s ON a.ItemID = s.ItemID;"""
+
 
 GET_CATEGORY_ID_QUERY = u"SELECT CategoryID FROM {0} WHERE ItemID = {1} ORDER BY DateRange"
 
